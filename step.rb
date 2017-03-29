182505547c92f3b380e7872244fef77174864823
Bartosz Górka <bartosz.gorka@droidsonroids.pl>
2017-03-29 16:45:58 +0200

    match fix

--- step.rb
+++ step.rb
@@ -3,6 +3,6 @@
 output = ""
 IO.popen('swiftlint').each do |line|
   puts line.chomp
-  output << line.chomp if line.chomp.match(/\(error\):/)
+  output << line.chomp if line.chomp.match(/(error:)/)
 end
 abort unless output.empty?
