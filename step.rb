12bc0a77833cd1d42e8b204887da17426e5b70df
Bartosz Górka <bartosz.gorka@droidsonroids.pl>
2017-03-30 10:19:31 +0200

    if statment for match + regex improvement

--- step.rb
+++ step.rb
@@ -2,7 +2,9 @@
 
 output = ""
 IO.popen('swiftlint').each do |line|
-  puts line.chomp
-  output << line.chomp if line.chomp.match(/(error:)/)
+  if line.chomp.match(/(\serror:\s)/)
+    puts line.chomp
+    output << line.chomp
+  end
 end
 abort unless output.empty?
